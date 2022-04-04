e0e9ed679d7cf71117ee5f57b842ee8228b29dd7
carleydegennaro <59845484+carleydegennaro@users.noreply.github.com>
2022-04-04 12:44:57 -0500

    Add files via upload

--- velocity.ino
+++ velocity.ino
@@ -1,73 +1,73 @@
 void setup() {
   // put your setup code here, to run once:
-const int xpin = A1;
-const int ypin = A2;
-const int zpin = A3;
-int sampleDelay = 100;
-
-float avg_x;
-float avg_y;
-float avg_z;
-
-float xval[100] = {0};
-float yval[100] = {0};
-float zval[100] = {0};
-float xavg, yavg, zavg;
-
-bool flag = false;
-
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Hold still for 5 seconds");
-  Serial.println("Begin calibrating");
-  analogReference(EXTERNAL); //3.3V pin
-  pinMode(xpin, INPUT);
-  pinMode(ypin, INPUT);
-  pinMode(zpin, INPUT);
-  int timeStart = millis();
-  int counter = 0;
-  float x = 0;
-  float y = 0;
-  float z = 0;
-  float zero_G =512;
-  float scale =102.3; 
-  while (timeStart < 5000) {
-    int x_raw = analogRead(xpin);
-    float x_adjust = (float(x_raw) - zero_G)/scale;
-    x = x + x_adjust;
-    delay(1);
-    int y_raw = analogRead(ypin);
-    float y_adjust = (float(y_raw) - zero_G)/scale;
-    y = y + y_adjust;
-    delay(1);
-    int z_raw = analogRead(zpin);
-    float z_adjust = (float(z_raw) - zero_G)/scale;
-    z = z + z_adjust;
-    delay(1);
-    timeStart = millis();
-    counter++;
-  }
-  Serial.println("Calibration Complete: Begin Moving");
-  avg_x = x/counter;
-  avg_y = y/counter;
-  avg_z = z/counter;
-  Serial.print("X offset: ");
-  Serial.print(avg_x);
-  Serial.print("\t");
-  Serial.print("Y offset: ");
-  Serial.print(avg_y);
-  Serial.print("\t");
-  Serial.print("Z offset: ");
-  Serial.print(avg_z);
-  Serial.println();
-  Serial.print("Time Stamp: ");
-  Serial.print('\t');
-  Serial.print("Acceleration: ");
-
-// initial conditions for calculating velocity
-  vox=0;
-  voy=0;
-  voz=0;
+  const int xpin = A1;
+  const int ypin = A2;
+  const int zpin = A3;
+  int sampleDelay = 100;
+  
+  float avg_x;
+  float avg_y;
+  float avg_z;
+  
+  float xval[100] = {0};
+  float yval[100] = {0};
+  float zval[100] = {0};
+  float xavg, yavg, zavg;
+  
+  bool flag = false;
+  
+  void setup() {
+    Serial.begin(9600);
+    Serial.println("Hold still for 5 seconds");
+    Serial.println("Begin calibrating");
+    analogReference(EXTERNAL); //3.3V pin
+    pinMode(xpin, INPUT);
+    pinMode(ypin, INPUT);
+    pinMode(zpin, INPUT);
+    int timeStart = millis();
+    int counter = 0;
+    float x = 0;
+    float y = 0;
+    float z = 0;
+    float zero_G =512;
+    float scale =102.3; 
+    while (timeStart < 5000) {
+      int x_raw = analogRead(xpin);
+      float x_adjust = (float(x_raw) - zero_G)/scale;
+      x = x + x_adjust;
+      delay(1);
+      int y_raw = analogRead(ypin);
+      float y_adjust = (float(y_raw) - zero_G)/scale;
+      y = y + y_adjust;
+      delay(1);
+      int z_raw = analogRead(zpin);
+      float z_adjust = (float(z_raw) - zero_G)/scale;
+      z = z + z_adjust;
+      delay(1);
+      timeStart = millis();
+      counter++;
+    }
+    Serial.println("Calibration Complete: Begin Moving");
+    avg_x = x/counter;
+    avg_y = y/counter;
+    avg_z = z/counter;
+    Serial.print("X offset: ");
+    Serial.print(avg_x);
+    Serial.print("\t");
+    Serial.print("Y offset: ");
+    Serial.print(avg_y);
+    Serial.print("\t");
+    Serial.print("Z offset: ");
+    Serial.print(avg_z);
+    Serial.println();
+    Serial.print("Time Stamp: ");
+    Serial.print('\t');
+    Serial.print("Acceleration: ");
+  
+    // initial conditions for calculating velocity
+    vox=0;
+    voy=0;
+    voz=0;
 }
 
 int timeStart = millis();
@@ -115,6 +115,10 @@ void loop() {
     Serial.print(currTime);
     Serial.print('\t');
     Serial.println(vx);
+
+    vox=vx
+    voy=vy
+    voz=vz
 
    delay(sampleDelay);
   
