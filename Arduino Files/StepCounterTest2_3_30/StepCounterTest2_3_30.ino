e29db1efc40ca1b2e146ac3ff4f564cf52d8f0e1
Gillian <gilliansaperstein@wustl.edu>
2022-04-13 20:12:58 -0500

    algorithm works??? **not clickbait**

--- Arduino Files/StepCounterTest2_3_30/StepCounterTest2_3_30.ino
+++ Arduino Files/StepCounterTest2_3_30/StepCounterTest2_3_30.ino
@@ -7,125 +7,15 @@ float avg_x;
 float avg_y;
 float avg_z;
 
-float xval[100] = {0};
-float yval[100] = {0};
-float zval[100] = {0};
+float xval;
+float yval;
+float zval;
 float xavg, yavg, zavg;
 
 float threshold = 0.6;
 float timeThreshold = 1000;
 
 bool flag = false;
-
-void setup() {
-  Serial.begin(9600);
-  analogReference(EXTERNAL); //3.3V pin
-  pinMode(xpin, INPUT);
-  pinMode(ypin, INPUT);
-  pinMode(zpin, INPUT);
-  calibrate();
-}
-
-unsigned long timeStart = millis();
-unsigned long currTime;
-int steps = 0;
-
-float baseline = 0;
-float baseCounter = 0;
-
-void loop() {
-  int acc = 0;
-  float totvect[100] = {0};
-  float totave[100] = {0};
-  float xaccl[100] = {0};
-  float yaccl[100] = {0};
-  float zaccl[100] = {0};
-  float zero_G =512;
-  float scale =102.3; 
-
-unsigned long timeOfCrossThreshold = 0;
-  for (int a = 0; a < 100; a++) {
-    int x_reading = analogRead(xpin);
-    float x_adjust = (float(x_reading) - zero_G)/scale;
-    float x_calib = x_adjust - avg_x;
-    delay(1); 
-  
-    int y_reading = analogRead(ypin);
-    float y_adjust = (float(y_reading) - zero_G)/scale;
-    float y_calib = y_adjust - avg_y;
-  
-    int z_reading = analogRead(zpin);
-    float z_adjust = (float(z_reading) - zero_G)/scale;
-    float z_calib = z_adjust - avg_z;
-    
-    xaccl[a] = x_calib;
-    delay(1);
-    yaccl[a] = y_calib;
-    delay(1);
-    zaccl[a] = z_calib;
-    delay(1);
-  
-    totvect[a] = sqrt(((xaccl[a] - xavg) * (xaccl[a] - xavg)) + ((yaccl[a] - yavg) * (yaccl[a] - yavg)) + ((zval[a] - zavg) * (zval[a] - zavg)));
-    totave[a] = (totvect[a] + totvect[a - 1]) / 2 ;
-    currTime = millis() - timeStart;
-  
-   if (totave[a] < totave[a - 1] + 0.02 && totave[a] > totave[a - 1] - 0.02) {
-      baseline = baseline + totave[a];
-      baseCounter = baseCounter + 1;
-  
-      if (baseCounter >= 1000) {
-        float avg = baseline / baseCounter;
-        Serial.print("New baseline is:");
-        Serial.println(avg);
-        baseline = 0;
-        baseCounter = 0;
-        threshold = 10* baseline;
-      }
-  }
-  
-  if (totave[a] > threshold && timeOfCrossThreshold == 0)
-  {
-    steps = steps + 1;
-    timeOfCrossThreshold = millis();
-
-//    Serial.print(currTime);
-//    Serial.print('\t');
-//    Serial.print(totave[a]);
-//    Serial.print('\t');
-//    Serial.println(steps);
-   }
-
-  else if (totave[a] > threshold)
-  {
-    if (millis() - timeOfCrossThreshold < timeThreshold) {
-      
-    }
-    else {
-      steps = steps + 1;
-      timeOfCrossThreshold = 0;
-    }
-  }
-
-  else
-  {
-    timeOfCrossThreshold = 0;
-  }
-
-  Serial.print(currTime);
-    Serial.print('\t');
-    Serial.print(totave[a]);
-    Serial.print('\t');
-    Serial.println(steps);
-
-
-  }
-
-   
-   //
-   // delay before next reading:
-   delay(sampleDelay);
-   
-}
 
 void calibrate() {
   Serial.println("Hold still for 5 seconds");
@@ -153,7 +43,6 @@ void calibrate() {
     delay(1);
     timeStart = millis();
     counter++;
-
   }
   
   avg_x = x/counter;
@@ -176,4 +65,98 @@ void calibrate() {
   Serial.println("");
   
   Serial.println("Calibration Complete: Begin Moving");
+}
+
+void setup() {
+  Serial.begin(9600);
+  analogReference(EXTERNAL); //3.3V pin
+  pinMode(xpin, INPUT);
+  pinMode(ypin, INPUT);
+  pinMode(zpin, INPUT);
+  calibrate();
+}
+
+unsigned long timeStart = millis();
+unsigned long currTime;
+int steps = 0;
+
+float baseline = 0;
+float baseCounter = 0;
+
+void loop() {
+  int acc = 0;
+
+  float totvect;
+  float totave;
+  float xaccl;
+  float yaccl;
+  float zaccl;
+  float zero_G =512;
+  float scale =102.3; 
+
+unsigned long timeOfCrossThreshold = 0;
+    int x_reading = analogRead(xpin);
+    float x_adjust = (float(x_reading) - zero_G)/scale;
+    float x_accl = x_adjust - avg_x;
+    delay(1); 
+  
+    int y_reading = analogRead(ypin);
+    float y_adjust = (float(y_reading) - zero_G)/scale;
+    float y_accl = y_adjust - avg_y;
+    
+    delay(1);
+    int z_reading = analogRead(zpin);
+    float z_adjust = (float(z_reading) - zero_G)/scale;
+    float z_accl = z_adjust - avg_z;
+
+
+    totave = sqrt(((xaccl - xavg) * (xaccl - xavg)) + ((yaccl - yavg) * (yaccl - yavg)) + ((zval - zavg) * (zval - zavg)));
+    currTime = millis() - timeStart;
+  
+//   if (totave[a] < totave[a - 1] + 0.02 && totave[a] > totave[a - 1] - 0.02) {
+//      baseline = baseline + totave[a];
+//      baseCounter = baseCounter + 1;
+//  
+//      if (baseCounter >= 1000) {
+//        float avg = baseline / baseCounter;
+//        Serial.print("New baseline is:");
+//        Serial.println(avg);
+//        baseline = 0;
+//        baseCounter = 0;
+//        threshold = 10* baseline;
+//      }
+//  }
+  
+  if (totave > threshold && timeOfCrossThreshold == 0)
+  {
+    steps = steps + 1;
+    Serial.println("Movement Detected!");
+    timeOfCrossThreshold = millis();
+   }
+
+  else if (totave > threshold)
+  {
+    if (millis() - timeOfCrossThreshold < timeThreshold) {
+      
+    }
+    else {
+      steps = steps + 1;
+      Serial.println("Movement Detected!");
+      timeOfCrossThreshold = 0;
+    }
+  }
+
+  else
+  {
+    timeOfCrossThreshold = 0;
+  }
+
+    Serial.print(currTime);
+    Serial.print('\t');
+    Serial.print(totave);
+    Serial.print('\t');
+    Serial.println(steps);
+
+   delay(sampleDelay);
+   
 }
